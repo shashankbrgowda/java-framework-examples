fce4fadf6138252a16f61841f3b53e9ce1a8f4fb
Shashank B R <shashank.b.r.gowda45587@gmail.com>
2022-05-30 15:03:16 +0100

    invoke and call examples

---
+++ mutiny-examples/src/main/java/org/shashank/mutiny/examples/InvokeAndCall.java
@@ -0,0 +1,40 @@
+package org.shashank.mutiny.examples;
+
+import io.smallrye.mutiny.Multi;
+import io.smallrye.mutiny.Uni;
+
+/*
+Invoke and call are used to peek the pipeline while processing the events and They dont have any effect on the
+event itself and they wont modify the original event
+ */
+public class InvokeAndCall {
+    public static void main(String[] args) {
+        // Invoke -> Invoke is mostly used for logging - This is synchronous, return type is void
+        // Call -> Closing resources and flushing - This is asynchronous, retunr type is Uni<?>
+
+        Multi.createFrom().items("A", "B", "C")
+                .onItem().transform(String::toLowerCase)
+                .onItem().invoke(i -> System.out.print(i + " "))
+                .subscribe().with(System.out::println);
+        /*
+        output:
+        a a
+        b b
+        c c
+         */
+
+        Multi.createFrom().items("A", "B", "C")
+                .onItem().transform(String::toLowerCase)
+                .onItem().call(i -> Uni.createFrom().item(i))
+                .subscribe().with(System.out::print);
+
+
+        Multi.createFrom().items("Shashank")
+                .onItem().invoke(i -> System.out.println(i))
+                .onFailure().invoke(f -> System.out.println())
+                .onCompletion().invoke(() -> System.out.println("Compelted"))
+                .onCancellation().invoke(() -> System.out.println("Cancelled"))
+                .onSubscription().invoke(() -> System.out.println("Subscription"))
+                .subscribe().with(System.out::println);
+    }
+}
